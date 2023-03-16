8cb75ff1e410c8f4fa3ca264663749a1af7084d3
aminulislam01101 <aminul.islam01101@gmail.com>
2023-03-16 22:37:16 +0600

    update 17

--- configs/passport.google.js
+++ configs/passport.google.js
@@ -39,9 +39,9 @@ passport.deserializeUser((id, done) => {
 passport.use(
   new GoogleStrategy(
     {
-      clientID: `${process.env.GOOGLE_CLIENT_ID}`,
-      clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
-      callbackURL: 'auth/google/callback',
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK,
     },
     async (accessToken, refreshToken, profile, done) => {
       // done(null, profile);
